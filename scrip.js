7f257c1ec1b2730fb30228894d5940e06b4f9f7a
Gustavo Henrique <gugatgg54@gmail.com>
2023-03-08 11:46:49 -0300

    :tada: initial commit

---
+++ scrip.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const hmtl= document.documentElement
+
+  // if(hmtl.classList.contains('light')) {
+  //  hmtl.classList.remove('light')
+  // } else {
+  //  hmtl.classList.add('light') }
+
+
+  hmtl.classList.toggle('light')
+}
+
+
+
